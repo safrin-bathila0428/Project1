9aeaa48fd42540b39f22bd3ab6afc2ec0d8eb9de
safrin-bathila0428 <67837199+safrin-bathila0428@users.noreply.github.com>
2020-07-07 16:16:32 +0530

    completed the project

---
+++ product of two numbers.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=5,b=7;
+	cout<<"product of"<<a<<"and"<<b<<"is"<<a*b;
+	return 0;
+}
---
+++ size of a variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"size of char:"<<sizeof(char)<<"byte";
+	cout<<"size of int:"<<sizeof(int)<<"bytes";
+	cout<<"size of float:"<<sizeof(float)<<"bytes";
+	cout<<"size of double:"<<sizeof(double)<<"bytes";
+	
+	return 0;
+}
